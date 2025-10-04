f87cc71456edf0b9dba143944a53f607ebe8dc15
mohammad <mhmdalhmrady@gmail.com>
2025-10-04 17:18:12 +0330

    Add Main Template &Install Packages

--- PackMarket/Program.cs
+++ PackMarket/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
