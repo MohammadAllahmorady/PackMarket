71826aed0c0811e76c7e7bca8ca06a85c39b679d
mohammad <mhmdalhmrady@gmail.com>
2025-10-05 20:45:18 +0330

    Create New Slider

--- PackMarket/Areas/Admin/Controllers/SliderController.cs
+++ PackMarket/Areas/Admin/Controllers/SliderController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PackMarket.Core.Services.Interfaces;
+using PackMarket.DataLayer.Entities;
 
 namespace PackMarket.Areas.Admin.Controllers
 {
@@ -15,6 +16,21 @@ namespace PackMarket.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_SlidrService.ShowAllSliders());
+        }
+        [HttpGet]
+        public IActionResult AddSlider()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSlider(MainSlider mainSlider)
+        {
+            if (!ModelState.IsValid)
+                return View(mainSlider);
+
+            int res = _SlidrService.AddSlider(mainSlider);
+            TempData["Result"]=res>0?"true":"false";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
